602c15445e16e1fe6f2dee43f8bd8aa3776b6e10
Pakorn Nathong <goddesign11294_2@hotmail.com>
2021-05-24 20:19:17 +0700

    added export to main library and add null check to getWeb3Provider

--- lib/flutter_web3_provider.dart
+++ lib/flutter_web3_provider.dart
@@ -3,7 +3,12 @@ library flutter_web3_provider;
 import 'ethereum.dart';
 import 'ethers.dart';
 
-Web3Provider getWeb3Provider() {
-  Web3Provider web3 = Web3Provider(ethereum);
-  return web3;
+export 'ethereum.dart';
+export 'ethers.dart';
+
+Web3Provider? getWeb3Provider() {
+  if (ethereum != null) {
+    Web3Provider web3 = Web3Provider(ethereum!);
+    return web3;
+  }
 }
