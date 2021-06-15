1321356334f167bcf37f886a919c0258f9a8e76d
Pakorn Nathong <goddesign11294_2@hotmail.com>
2021-06-15 19:58:48 +0700

    add ERC20 contract wrapper and more utility function

--- lib/src/ethers/ethers_utils.dart
+++ lib/src/ethers/ethers_utils.dart
@@ -1,22 +1,130 @@
+import '../objects/objects.dart';
+import '../utils.dart';
 import 'ethers.dart';
+import 'ethers_wrapper.dart';
 
-/// Get default [AbiCoder]
+/// Get default [AbiCoder].
 AbiCoder get abiCoder => Utils.defaultAbiCoder;
 
-/// Convert JS [BigNumber] to Dart [BigInt]
+/// Convert JS [BigNumber] to Dart [BigInt].
 BigInt bigNumberToBigInt(BigNumber bigNumber) =>
     BigInt.parse(bigNumber.toString());
 
-/// Construct [Contract] with [Signer] from default [Provider]
+/// Construct [Contract] with [Signer] from default [Provider].
 Contract defaultContractWithSigner(String address, List<String> abi) =>
     Contract(address, abi, provider!.getSigner());
 
-extension BigNumberExt on BigNumber {
-  /// Convert JS [BigNumber] to Dart [BigInt]
-  BigInt get toBigInt => bigNumberToBigInt(this);
+/// Dart Class for ERC20 Contract, A standard API for tokens within smart contracts.
+///
+/// This standard provides basic functionality to transfer tokens, as well as allow tokens to be approved so they can be spent by another on-chain third party
+class ContractERC20 {
+  /// Minimal abi interface of ERC20
+  static const abi = [
+    'function name() view returns (string)',
+    'function symbol() view returns (string)',
+    'function balanceOf(address) view returns (uint)',
+    'function decimals() view returns (uint)',
+    'function totalSupply() view returns (uint)',
+    'function allowance(address owner, address spender) external view returns (uint256)',
+    'function transfer(address, uint)',
+    'function transferFrom(address, address, uint)',
+    'function approve(address, uint256)',
+    'event Transfer(address indexed from, address indexed to, uint amount)',
+    'event Approval(address indexed owner, address indexed spender, uint256 value)'
+  ];
+
+  /// Ethers Contract object.
+  final Contract contract;
+
+  /// True if [Signer] is attached to [contract].
+  final bool isReadOnly;
+
+  /// Instantiate ERC20 Contract using default abi.
+  ///
+  /// [isReadOnly] is determined by whether `providerOrSigner` is [Signer] or not.
+  ContractERC20(String address, dynamic providerOrSigner)
+      : assert(providerOrSigner != null, 'providerOrSigner should not be null'),
+        assert(address.isNotEmpty, 'address should not be empty'),
+        assert(Utils.isAddress(address), 'address should be '),
+        contract = Contract(address, abi, providerOrSigner),
+        isReadOnly = !(providerOrSigner is Signer);
+
+  /// Returns the number of decimals used to get its user representation.
+  ///
+  /// For example, if `decimals` equals `2`, a balance of `505` tokens should
+  /// be displayed to a user as `5,05` (`505 / 10 ** 2`).
+  ///
+  /// Tokens usually opt for a value of 18, imitating the relationship between
+  /// Ether and Wei. This is the value `ERC20` uses, unless this function is
+  /// overridden
+  Future<int> get decimals async =>
+      (await contract.call<BigNumber>('decimals')).toInt;
+
+  /// Returns the name of the token.
+  Future<String> get name => contract.call<String>('name');
+
+  /// Returns the symbol of the token, usually a shorter version of the name.
+  Future<String> get symbol => contract.call<String>('symbol');
+
+  /// Returns the amount of tokens in existence.
+  Future<BigInt> get totalSupply async =>
+      (await contract.call<BigNumber>('totalSupply')).toBigInt;
+
+  /// Returns the remaining number of tokens that [spender] will be allowed to spend on behalf of [owner] through `transferFrom`.
+  ///
+  /// This is zero by default.
+  Future<BigInt> allowance(String owner, String spender) async =>
+      (await contract.call<BigNumber>('allowance', [owner, spender])).toBigInt;
+
+  /// Sets [amount] as the allowance of [spender] over the caller's tokens.
+  Future<TransactionResponse> approve(String spender, BigInt amount) =>
+      contract.send('approve', [spender, amount.toString()]);
+
+  /// Transfer token from `msg.sender` to [recipient] in [amount]. Emits `Transfer` events when called.
+  Future<TransactionResponse> transfer(String recipient, BigInt amount) =>
+      contract.send('transfer', [recipient, amount.toString()]);
+
+  /// Transfer token from [sender] to [recipient] in [amount]. Emits `Transfer` events when called.
+  Future<TransactionResponse> transferFrom(
+          String sender, String recipient, BigInt amount) =>
+      contract.send('transfer', [sender, recipient, amount.toString()]);
+
+  /// Emitted when the allowance of a `spender` for an `owner` is set by a call to `approve`.
+  ///
+  /// `value` is the new allowance.
+  void onApproval(
+          void Function(
+                  String owner, String spender, BigInt value, dynamic data)
+              callback) =>
+      contract.on(
+          'Approval',
+          (String owner, String spender, BigNumber value, dynamic data) =>
+              callback(owner, spender, value.toBigInt, convertToDart(data)));
+
+  /// Emitted when `amount` tokens are moved from one account (`from`) to another (`to`).
+  ///
+  /// Note that `amount` may be zero.
+  void onTransfer(
+          void Function(String from, String to, BigInt amount, dynamic data)
+              callback) =>
+      contract.on(
+          'Transfer',
+          (String from, String to, BigNumber amount, dynamic data) =>
+              callback(from, to, amount.toBigInt, convertToDart(data)));
 }
 
 extension BigIntExt on BigInt {
-  /// Convert Dart [BigInt] to JS [BigNumber]
+  /// Convert Dart [BigInt] to JS [BigNumber].
   BigNumber get toBigNumber => BigNumber.from(this.toString());
+}
+
+extension BigNumberExt on BigNumber {
+  /// Convert JS [BigNumber] to Dart [BigInt].
+  BigInt get toBigInt => BigInt.parse(this.toString());
+
+  /// Convert JS [BigNumber] to Dart [int].
+  int get toInt => int.parse(this.toString());
+
+  /// Convert JS [BigNumber] to Dart [double].
+  double get toDouble => double.parse(this.toString());
 }
--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,4 +2,5 @@ import 'dart:convert';
 
 import 'ethereum/ethereum.dart';
 
+/// Convert JS object to Dart object to avoid type error.
 dynamic convertToDart(dynamic jsObject) => json.decode(stringify(jsObject));
