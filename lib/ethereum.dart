59474b6d13473f0fb45fb65f7986d858c62cb81f
Pakorn Nathong <goddesign11294_2@hotmail.com>
2021-07-08 15:40:39 +0700

    export directory fix

--- lib/ethereum.dart
+++ lib/ethereum.dart
@@ -2,3 +2,4 @@ library ethereum;
 
 export 'src/ethereum/ethereum.dart'
     hide EthereumBaseImpl, defaultProviderImpl, getEthereumImpl;
+export 'src/ethereum/utils.dart';
--- lib/ethers.dart
+++ lib/ethers.dart
@@ -1,4 +1,4 @@
 library ethers;
 
 export 'src/ethers/ethers.dart';
-export 'src/ethers/ethers_utils.dart';
+export 'src/ethers/utils.dart';
