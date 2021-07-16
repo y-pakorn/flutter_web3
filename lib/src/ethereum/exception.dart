be09188aa15cda38b75addb9862b5be90840f197
Pakorn Nathong <goddesign11294_2@hotmail.com>
2021-07-16 15:12:40 +0700

    override toString method to reflect error more clearly

--- lib/src/ethereum/exception.dart
+++ lib/src/ethereum/exception.dart
@@ -2,6 +2,13 @@ class EthereumUnrecognizedChainException implements Exception {
   final int chainId;
 
   EthereumUnrecognizedChainException(this.chainId);
+
+  @override
+  String toString() =>
+      'EthereumUnrecognizedChainException: Chain $chainId is not recognized, please add the chain using `walletAddChain` first';
 }
 
-class EthereumUserRejected implements Exception {}
+class EthereumUserRejected implements Exception {
+  @override
+  String toString() => 'EthereumUserRejected: User rejected the request';
+}
