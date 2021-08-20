1a3f7ee3b7a25ab2ff4fc3210a6a70f09d37c0ac
Pakorn Nathong <goddesign11294_2@hotmail.com>
2021-08-20 12:31:31 +0700

    add AccessList constructor example

--- lib/src/ethers/access_list.dart
+++ lib/src/ethers/access_list.dart
@@ -6,6 +6,17 @@ part of ethers;
 class AccessList extends Interop<_AccessListImpl> {
   const AccessList._(_AccessListImpl impl) : super.internal(impl);
 
+  /// Create new [AccessList] from [address] and [storageKeys], [storageKey] elements must be empty or bytes32 string.
+  ///
+  /// ---
+  ///
+  /// final accessList = AccessList(
+  ///   '0x0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  ///   [
+  ///     "0x0000000000000000000000000000000000000000000000000000000000000004",
+  ///     "0x0bcad17ecf260d6506c6b97768bdc2acfb6694445d27ffd3f9c1cfbee4a9bd6d"
+  ///   ],
+  /// );
   factory AccessList(String address, List<String> storageKeys) =>
       AccessList._(_AccessListImpl(address, storageKeys));
 
