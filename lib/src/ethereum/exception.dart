f5c90bba0376e7320a369e681456fde4add4cabd
Pakorn Nathong <goddesign11294_2@hotmail.com>
2021-11-14 18:33:53 +0700

    fix: correct EthereumException inheritance

--- lib/src/ethereum/exception.dart
+++ lib/src/ethereum/exception.dart
@@ -1,24 +1,29 @@
-class EthereumUnrecognizedChainException implements Exception {
+class EthereumException implements Exception {
+  final int code;
+  final String message;
+
+  const EthereumException(this.code, this.message);
+
+  @override
+  String toString() => 'EthereumException: $code $message';
+}
+
+class EthereumUnrecognizedChainException extends EthereumException {
   final int chainId;
 
-  EthereumUnrecognizedChainException(this.chainId);
+  const EthereumUnrecognizedChainException(this.chainId,
+      [int code = 4902, String message = ''])
+      : super(code, message);
 
   @override
   String toString() =>
       'EthereumUnrecognizedChainException: Chain $chainId is not recognized, please add the chain using `walletAddChain` first';
 }
 
-class EthereumUserRejected implements Exception {
+class EthereumUserRejected extends EthereumException {
+  const EthereumUserRejected([int code = 4001, String message = ''])
+      : super(code, message);
+
   @override
   String toString() => 'EthereumUserRejected: User rejected the request';
-}
-
-class EthereumException implements Exception {
-  final int code;
-  final String message;
-
-  EthereumException(this.code, this.message);
-
-  @override
-  String toString() => 'EthereumException: $code $message';
 }
